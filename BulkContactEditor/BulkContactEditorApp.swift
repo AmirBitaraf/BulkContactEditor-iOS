1d6ec3d241e8e82e28e5baa0b5ad9a01eea8806e
Amir Bitaraf <8049618+AmirBitaraf@users.noreply.github.com>
2023-11-23 23:24:57 +0100

    Add base project

---
+++ BulkContactEditor/BulkContactEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulkContactEditorApp.swift
+//  BulkContactEditor
+//
+//  Created by Amir Bitaraf on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BulkContactEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
